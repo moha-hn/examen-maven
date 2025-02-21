58b9e82f2dc6c3e8db7850fa256754d9bac34f8e
Mohamed Amine Hanou <2417033@grasset.qc.ca>
2025-02-21 13:43:22 -0500

    initial commit

---
+++ src/main/java/com/mycompany/examen_maven/Examen_maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_maven;
+
+/**
+ *
+ * @author 2417033
+ */
+public class Examen_maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
